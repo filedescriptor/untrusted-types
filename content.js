b6929d2a5e7126c22ee48bd9395bc1cb736d001f
filedescriptor <filedescriptor@gmail.com>
2020-11-15 18:17:12 +0800

    Add files via upload

---
+++ content.js
@@ -0,0 +1,49 @@
+// chrome.storage is too slow to allow custom options
+// edit the settings here instead
+let keyword = 'd0mxss';
+let onlyLogHighlighted = false;
+// creating a policy in the content script context will result in Chrome crashing
+// therefore we need to run the code in the website's context
+const head = document.createElement('head');
+const meta = document.createElement('meta');
+const script = document.createElement('script');
+script.innerHTML = `
+(() => {
+    function log(input, type, sink) {
+        let keyword = ${JSON.stringify(keyword)};
+        if (keyword && input.includes(keyword)) {
+            let highlightedInput = input.replaceAll(keyword, '%c$&%c');
+            let args = [
+                location.href + '\\n%c' + sink + '\\n%c' + highlightedInput, 
+                'background: red; color: white; font-size: 16px',
+                'background: unset; color: unset; font-size: unset;'
+            ];
+            for (let i = 0; i < input.split(keyword).length - 1; i++) {
+                args.push('color: red; border: 1px dotted red; background: yellow;');
+                args.push('color: unset; border: reset;background: unset');
+            }
+            console.trace.apply(console, args);
+        } else if (${!onlyLogHighlighted}) {
+            console.trace(location.href + '\\n%c' + sink, 'background: #222; color: #bada55; font-size: 16px', '\\n' + input);
+        }
+        return input;
+    }
+    if (!trustedTypes.defaultPolicy) {
+        trustedTypes.createPolicy('default', {
+            createHTML: log,
+            createScript: log,
+            createScriptURL: log
+        });
+    } else {
+        console.warn('One or more documents are using Trusted Types. Untrusted Types is disabled.')
+    }
+})();
+//@ sourceURL=UNTRUSTED_TYPES_CHECK_STACK_BELOW
+`;
+
+meta.httpEquiv = 'Content-Security-Policy';
+meta.content = "require-trusted-types-for 'script'";
+head.appendChild(script);
+head.appendChild(meta);
+document.documentElement.appendChild(head);
+head.remove();
