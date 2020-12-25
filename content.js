93202cb33d4d4a7f3c115c8e15a013258b919dc2
Caio Araújo <caio.araujo@PPM-SPO-02577.local>
2020-12-25 20:48:25 -0300

    Adding more options

--- content.js
+++ content.js
@@ -1,8 +1,14 @@
 // chrome.storage is too slow to allow custom options
 // edit the settings here instead
-let keyword = 'd0mxss';
-let trace_limit = -1 ; // -1 = no limit
-let onlyLogHighlighted = false;
+
+options = {
+    "keyword" : 'd0mxss',
+    "traceLimit" : 5, // 9999 = no limit
+    "onlyLogHighlighted" : false,
+    "ignoredDomains" : [], // Regex list of domains to ignore
+    "ignoredFiles" : [] // Regex list of files to ignore
+}
+
 // creating a policy in the content script context will result in Chrome crashing
 // therefore we need to run the code in the website's context
 const head = document.createElement('head');
@@ -16,26 +22,25 @@ script.innerHTML = `
         _open.apply(window, arguments);
     }
     function log(input, type, sink) {
-        let keyword = ${JSON.stringify(keyword)};
-        if (keyword && input.includes(keyword)) {
-            let highlightedInput = input.replaceAll(keyword, '%c$&%c');
+        let options = ${JSON.stringify(options)};
+        if (options.keyword && input.includes(options.keyword)) {
+            let highlightedInput = input.replaceAll(options.keyword, '%c$&%c');
             let args = [
                 location.href + '\\n%c' + sink + '\\n%c' + highlightedInput, 
                 'background: red; color: white; font-size: 16px',
                 'background: unset; color: unset; font-size: unset;'
             ];
-            for (let i = 0; i < input.split(keyword).length - 1; i++) {
+            for (let i = 0; i < input.split(options.keyword).length - 1; i++) {
                 args.push('color: red; border: 1px dotted red; background: yellow;');
                 args.push('color: unset; border: reset;background: unset');
             }
             console.trace.apply(console, args);
-        } else if (${!onlyLogHighlighted}) {
+        } else if (!options.onlyLogHighlighted) {
             e = new Error();
             trace_n = (e.stack.match(/\\sat\\s/g) || []).length ; // bugged ?
-            console.log(trace_n);
-            if ( trace_n < ${trace_limit} ) {
-						     console.trace(location.href + '\\n%c' + sink, 'background: #222; color: #bada55; font-size: 16px', '\\n' + input);
-						}
+            if ( trace_n < options.traceLimit ) {
+                console.trace(location.href + '\\n%c' + sink, 'background: #222; color: #bada55; font-size: 16px', '\\n' + input);
+            }
         }
         return input;
     }
